122ed480650ec5f3ff28b862fb82a6667ae606ac
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-10-16 19:50:52 +0800

    feat: autoresize textarea height for longer text

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import { cn } from '@/lib/utils';
 
-function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
+function Textarea({
+  className,
+  ...props
+}: Omit<
+  React.ComponentPropsWithoutRef<'textarea'>,
+  'form' | 'slot' | 'style'
+>) {
   return (
-    <textarea
+    <TextareaAutosize
       data-slot="textarea"
       className={cn(
         'flex field-sizing-content min-h-25 w-full resize-none rounded-xs border border-slate-300 bg-slate-50 p-[15px] text-base transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
